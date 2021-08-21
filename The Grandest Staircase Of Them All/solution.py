5b34c2f89337cc4023f6471376441811d55d82e2
Nguyen Minh <73292024+minhntq237@users.noreply.github.com>
2021-08-21 18:08:32 +0800

    Add files via upload

---
+++ The Grandest Staircase Of Them All/solution.py
@@ -0,0 +1,12 @@
+def solution(n):
+    memo = [[0 for i in range(n + 1)] for j in range(n + 1)]
+    memo[0][0] = 1 #At height 0, bricks = 0, count as 1 way 
+    for height in range(1, n+1): #Ignore height 0
+        for numBricks in range(0, n+1):
+            #Q(n, k) = Q(n-k, k) + Q(n-k, k-1) for n>k>=1, with Q(1, 1)=1, Q(n, 0)=0 (n>=1). - Paul D. Hanna, Mar 04 2005
+            memo[height][numBricks] = memo[height - 1][numBricks]
+            if numBricks >= height:
+                memo[height][numBricks] += memo[height - 1][numBricks - height]
+    return memo[n][n] - 1
+
+print(solution(200))
